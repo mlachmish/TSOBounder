969a24d4dd11ba3d4f9b52bb826f04ac02f75b7d
Matan Lachmish <matan.lachmish@jivesoftware.com>
2016-08-04 21:39:48 +0300

    Flood TSO

--- bounder.py
+++ bounder.py
@@ -3,13 +3,14 @@
 import sys
 import time
 import matplotlib.pyplot as plt
+import numpy
 
 __author__ = "Matan Lachmish and Asaf Rokach"
 __copyright__ = "Copyright 2016, Bounding TSO"
 __version__ = "1.2"
 __status__ = "Development"
 
-kNumberOfTests = 100000
+kNumberOfTests = 1000
 kNumberOfWritesPerTests = 2 ** 10
 MeasureMethod = 'coarse'  # 'fine'/'coarse'
 
@@ -33,9 +34,13 @@ def main(argv):
         # Coarse Grained
         print('Calculating TSO size (CoarseGrained)...')
         sizeArray, meanTime = calculateTSOSizeCoarseGrained()
+        linearPlot = numpy.poly1d(numpy.polyfit(sizeArray, meanTime, 1))(sizeArray)
+        stepsPlot = [1 if x>linearPlot[i] else 0 for i, x in enumerate(meanTime)]
 
         print('Plotting Coarse Grained results')
         plt.plot(sizeArray, meanTime)
+        plt.plot(sizeArray, linearPlot)
+        plt.plot(sizeArray, stepsPlot)
         plt.title('Coarse Grained results')
         plt.ylabel('Process Time')
         plt.xlabel('# Bytes Written')
@@ -44,12 +49,33 @@ def main(argv):
     else:
         print('Unsupported MeasureMethod')
 
+def floodTSO():
+    meanWriteTime = 0.0
+    for test in range(1,kNumberOfTests):
+        storage = bytearray()  # New empty byte array
+        for byteNumber in range(1, kNumberOfWritesPerTests):
+            startTime = time.process_time()
+            storage.extend(b"x")
+            currentTime = time.process_time()
+            currentWriteTime = currentTime - startTime
+            if currentWriteTime >= meanWriteTime * 1.8 and byteNumber > 1: #Check if current write time pass the threshold (1.8)
+                print("Flooded at byte number: " + str(byteNumber))
+                return True
+            else:
+                meanWriteTime = ((meanWriteTime * (byteNumber - 1)) + currentWriteTime) / byteNumber
+    return False
 
 def calculateTSOSizeCoarseGrained():
     sizeArray = list(range(1,kNumberOfWritesPerTests))
     meanTimeArray = []
 
     for size in sizeArray:
+
+        #Flood TSO
+        if not floodTSO():
+            print("Flooding the TSO failed!")
+            return
+
         startTime = time.process_time()
         for k in range(1, kNumberOfTests):
             storage = bytearray()  # New empty byte array
@@ -67,6 +93,12 @@ def calculateTSOSizeFineGrained():
 
     for test in range(1,kNumberOfTests):
         storage = bytearray()  # New empty byte array
+
+        #Flood TSO
+        if not floodTSO():
+            print("Flooding the TSO failed!")
+            return
+
         timeLogger = [time.process_time()]
         for byteNumber in range(1, kNumberOfWritesPerTests):
             storage.extend(b"x")
